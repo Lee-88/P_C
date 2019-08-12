9f363983fde79bef23d320a5ddad2ba317ea80d6
Trevor <Trevor@DESKTOP-8RV6KN2.Home>
2019-08-12 20:47:11 +0100

    New Event Added

--- src/soft/circle_class.java
+++ src/soft/circle_class.java
@@ -7,13 +7,13 @@ import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 public class circle_class {
-		public static void createnew(Color xColor, int x, int y){
+		public static void createnew(Color xColor, double d, double e){
 			Circle cir2 = new Circle();
 			cir2.setRadius(50.0);
 			cir2.setFill(xColor);
 			window_class.layout.getChildren().addAll(cir2);
-			cir2.setLayoutX(x);
-			cir2.setLayoutY(y);
+			cir2.setLayoutX(d);
+			cir2.setLayoutY(e);
 			ScaleTransition tranCir = new ScaleTransition(); 
 			tranCir.setDuration(Duration.seconds(4));
 			tranCir.setByX(0.5f);
@@ -25,6 +25,6 @@ public class circle_class {
 		}
 		circle_class(){
 		//constructor
-			System.out.println("This Created A Circle 'o'");
+		System.out.println("This Created A Circle 'o'");
 		}
 }
